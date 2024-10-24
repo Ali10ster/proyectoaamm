8004639a4ec71c9e4e1f4c077e5edd04bdce1cf5
Ali10ster <alisteralbertomartinmiranda@gmail.com>
2024-10-24 09:45:57 +0100

    login sin funcionalidad

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,22 +1,47 @@
+import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Grid2'
+import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Button from '@mui/material/Button'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import "../index.css"
 function Login() {
-  
-    return (
-      <>
-      <Typography variant='h1'>Pagína login Alister Alberto Martin Miranda</Typography>
-      <Container role="main">
-        <Typography variant='h2' color='success'>Usamos h2</Typography>
-        <Typography variant='h3' color='secondary'>Usamos h3</Typography>
-        <Typography variant='h4' color='error'>Usamos h4</Typography>
-        <Typography variant='subtitle1' color='info'>Usamos subtitle</Typography>
-        <Button variant='text'>Boton 1</Button>
-        <Button variant='contained'>Boton 2</Button>
-        <Button variant='outlined'>Boton 3</Button>
-      </Container>
-      </>
-    )
-  }
-  
-  export default Login
+  const bduser='alister'
+  const bdpasswd='1234'
+  const handleSubmit = (e:any) => {
+    //Para que no mande el formulario, sino que haga lo que yo le diga.
+    e.preventDefault()
+    //Aquí iría el código que quiero que se ejecute
+    }
+    
+  return (
+    <>
+
+
+      <Paper elevation={12}>
+
+        <Box component="form" onSubmit={handleSubmit}>
+          <Grid container alignContent={'center'} alignItems={'center'}>
+            <Grid size={12}>
+              <Typography><h1>Sistema de acceso</h1></Typography>
+              <AccountCircleIcon class="tamaño"></AccountCircleIcon>
+            </Grid>
+            <Grid padding={2} size={12}>
+              <TextField label="Usuario" required fullWidth></TextField>
+            </Grid>
+            <Grid padding={2} size={12}>
+              <TextField label="Contraseña" required fullWidth></TextField>
+            </Grid>
+            <Grid padding={2} size={12}>
+              <Button variant="contained" type='submit'fullWidth>Acceder</Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Paper>
+    </>
+  )
+}
+
+export default Login
